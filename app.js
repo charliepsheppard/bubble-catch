fe5f0d0f664bc431cf2d0537f098ced0fb3144df
Charlie Sheppard <sheppard.charlie24@gmail.com>
2020-11-30 22:35:29 -0500

    Updated bubble/player collission detection

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ const canvasPosition = getPosition(canvas);
 
 canvas.addEventListener('mousemove', setMouseMove, false)
 
+// Utility Functions
+
 function setMouseMove(event) {
     mouse.x = event.clientX - canvasPosition.x;
     mouse.y = event.clientY - canvasPosition.y;
@@ -40,6 +42,17 @@ function getPosition(el) {
     };
 }
 
+const getDistance = (x1, y1, x2, y2) => {
+    let xDistance = x2 - x1;
+    let yDistance = y2 - y1;
+
+    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+}
+
+const randomIntegerFromRange = (min, max) => {
+    return Math.random() * (max - min * 1) + min;
+}
+
 canvas.addEventListener('resize', () => {
     canvas.width = innerWidth;
     canvas.height = innerHeight;
@@ -47,7 +60,9 @@ canvas.addEventListener('resize', () => {
     init();
 })
 
-class Bubble {
+// Objects
+
+class Player {
     constructor(x, y, radius, color) {
         this.x = x;
         this.y = y;
@@ -66,16 +81,76 @@ class Bubble {
     }
 
     update() {
+        this.draw();
+    }
+}
+class Bubble {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.strokeStyle = this.color;
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    update(bubbles) {
         this.draw()
+        // console.log(bubbles.length);
+
+        for (let i = 0; i < bubbles.length; i++) {
+            if(this === bubbles[i]) continue;
+            if (getDistance(this.x, this.y, bubbles[i].x, bubbles[i].y) - this.radius * 2 < 0) {
+                console.log('Has collided');
+            }
+            // console.log(bubbles[i]);
+        }
     }
 }
 
-let player;
+// Implementation 
 
-const init = () => {
-    player = new Bubble(200, 100, 50, 'dodgerblue');
+let player;
+let bubbles;
+
+function init() {
+    player = new Player(200, 100, 50, 'dodgerblue');
     player.draw();
+
+    bubbles = [];
+
+    for (let i = 0; i < 5; i++) {
+        const radius = 20;
+        let x = randomIntegerFromRange(radius, canvas.width - radius);
+        let y = randomIntegerFromRange(radius, canvas.height - radius);
+        const color = 'tomato';
+
+        if(i !== 0) {
+            for(let j = 0; j < bubbles.length; j++) {
+                if (getDistance(x, y, bubbles[j].x, bubbles[j].y) - radius * 2 < 0) {
+                    x = randomIntegerFromRange(radius, canvas.width - radius);
+                    y = randomIntegerFromRange(radius, canvas.height - radius);
+
+                    j = -1;
+                }
+            }
+        }
+
+        bubbles.push(new Bubble(x, y, radius, color))
+    }
+    // console.log(bubbles);
 }
+
+
+// Animation loop
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -84,6 +159,17 @@ function animate() {
     player.update();
 
     requestAnimationFrame(animate);
+
+    bubbles.forEach(bubble => {
+        // console.log(bubbles);
+        if (getDistance(player.x, player.y, bubble.x, bubble.y) < player.radius + bubble.radius) {
+            bubble.color = 'purple';
+        } else {
+            bubble.color = 'tomato';
+        }
+        bubble.update(bubbles);
+        // console.log(bubbles.length)
+    })
 
 }
 
